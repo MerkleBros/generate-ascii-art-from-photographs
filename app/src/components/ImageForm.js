2c4565d43d46d537bc5ad372b112306943c87a47
Patrick McCarver <patrick@merklebros.com>
2020-01-03 15:54:33 -0500

    Temp commit to stash changes while looking at broken Serverless API

--- app/src/components/ImageForm.js
+++ app/src/components/ImageForm.js
@@ -13,10 +13,40 @@ export default function ImageForm(props) {
     console.log("In handleChange, image: ", images)
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("in handleSubmit")
-    // Details for the api
+
+    const formData = new FormData()
+    formData.append('content', images.files[0])
+
+    let reader = new FileReader();
+    reader.readAsDataURL(images.files[0]);
+    reader.addEventListener("load", async function () {
+      // convert image file to base64 string
+
+      console.log("reader.result: ", reader.result)
+      console.log("formData: ", formData)
+      console.log('images.files[0]: ', images.files[0])
+      console.log('images.urls[0]: ', images.urls[0])
+      try {
+        const response = await fetch(
+          "https://cors-anywhere.herokuapp.com/https://r76qcc9zjc.execute-api.us-east-1.amazonaws.com/dev/generate_ascii"
+          , { method: 'POST'
+            , mode: 'cors' // no-cors, *cors, same-origin
+            , isBase64Encoded: true
+            , headers: 
+              { 'Content-Type': 'application/x-www-form-urlencoded'
+              , 'Accept':       'image/png;base64'
+              }
+            , body: reader.result // body data type must match "Content-Type" header
+            }); 
+
+        console.log(response)
+      } catch(e) {
+        console.log('error in fetch: ', e)
+      }
+    }, false);
   };
 
   return (
