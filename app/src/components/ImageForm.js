dc76f84d74ef34bd088cdb2a822b393bc81fb763
Patrick McCarver <patrick@merklebros.com>
2020-01-05 17:13:57 -0500

    Cleaning up Imageform more

--- app/src/components/ImageForm.js
+++ app/src/components/ImageForm.js
@@ -1,79 +1,85 @@
 import React, { useState } from "react";
-import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
+import uuidv1 from 'uuid/v1';
 
 export default function ImageForm(props) {
   const [images, setImages] = useState([]);
 
-  const handleChange = e => {
-    const thumbnailHeight = props.thumbnailHeight
-    const files = Array.from(e.target.files)
+  const handleChange =
+    event => {
+      const thumbnailHeight = props.thumbnailHeight
+      const files = Array.from(event.target.files)
 
-    const previewImages = 
-      files
-        .map(file => {
-          const image = new Image();
-          image.src = URL.createObjectURL(file);
-          return { file: file
-                 , src: image.src
-                 , thumbnail: image.src
-                 , thumbnailHeight: thumbnailHeight
-                 , thumbnailWidth: (image.width / image.height) * thumbnailHeight
+      const imageObjects = 
+        files
+          .map(file => {
+            const image = new Image();
+            image.src = URL.createObjectURL(file);
+            return { file: file
+                   , src: image.src
+                   , thumbnail: image.src
+                   , thumbnailHeight: thumbnailHeight
+                   , thumbnailWidth: (image.width / image.height) * thumbnailHeight
     }});
 
-    setImages(previewImages)
+    setImages(imageObjects)
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const postFileToAPI = 
+    async (file) => {
+      console.log("Posting file to Serverless API");
+      return await fetch(
+        "https://cors-anywhere.herokuapp.com/" +
+        "https://ovr5yyzvza.execute-api.us-east-1.amazonaws.com/dev/generate_ascii", 
+        { method: 'POST'
+        , mode: 'cors'
+        , headers: 
+          { 'Content-Type': 'image/jpg'
+          , 'Accept': 'image/png'
+          }
+        , body: file
+    }); 
+  };
 
-    console.log("in handleSubmit")
+  const createImageObjectFromResponse =
+    async (response) => {
+      console.log("Proccessing response from Serverless API");
 
-    let reader = new FileReader();
-    reader.readAsDataURL(images[0].file);
-    reader.addEventListener("load", async function () {
-      // convert image file to base64 string
+      const blob = await response.blob()
+      const file = new File([blob], uuidv1())
+      const url = URL.createObjectURL(file)
+      const image = new Image()
+      image.src = url
 
-      console.log("reader.result: ", reader.result)
-      const result = reader.result.substring(reader.result.indexOf(",") + 1)
-      console.log("result: ", result)
-      console.log('images[0].file: ', images[0].file)
-      console.log('images[0].src: ', images[0].src)
+      console.log("response blob: ", blob)
+      console.log("file: ", file)
+      console.log("url: ", url)
+
+      return { file: file
+             , src: image.src
+             , thumbnail: image.src
+             , thumbnailHeight: props.thumbnailHeight
+             , thumbnailWidth: (image.width / image.height) * props.thumbnailHeight
+  }}
+
+  const handleSubmit =
+    async (event) => {
+      event.preventDefault();
+
       try {
-        const response = await fetch(
-          "https://cors-anywhere.herokuapp.com/https://ovr5yyzvza.execute-api.us-east-1.amazonaws.com/dev/generate_ascii"
-          , { method: 'POST'
-            , mode: 'cors' // no-cors, *cors, same-origin
-            , headers: 
-              { 'Content-Type': 'image/jpg'
-              , 'Accept': 'image/png'
-              }
-            , body: images[0].file // body data type must match "Content-Type" header
-            }); 
+        for (const image of images) {
+          const response = await postFileToAPI(image.file)
+          const imageObject = await createImageObjectFromResponse(response)
 
-        console.log("response:")
-        console.log(response)
-        const blob = await response.blob()
-        console.log("response blob: ", blob)
-        const file = new File([blob], "Response.png")
-        console.log("file: ", file)
-        const url = URL.createObjectURL(file)
-        console.log("url: ", url)
+          console.log("response object: ", response)
+          console.log("ImageObject inside handleSubmit: ", imageObject)
 
-        const img = new Image()
-        img.src = url
-        document.body.append(img)
-
-        const link = document.createElement("a") 
-        link.download = "response.png"
-        link.href = url;
-        link.text = "Download here"
-        document.body.append(link)
-        
+          setImages([...images, imageObject])
+        }
       } catch(e) {
-        console.log('error in fetch: ', e)
+        const message = "Error processing file in API: "
+        console.log(message, e)
       }
-    }, false);
   };
 
   return (
@@ -86,12 +92,15 @@ export default function ImageForm(props) {
             multiple
             accept='.png, .jpg, .jpeg'
             id='image-input'
-            onChange={e => handleChange(e)}
-          />
+            onChange={e => handleChange(e)} />
         </label>
-        <input type="submit" value="Submit"/>
+        <input 
+          type="submit" 
+          value="Submit"/>
       </form>
-      <Gallery images={images} />
+      <Gallery 
+        images={images} 
+        backdropClosesModal={true} />
     </>
   );
 }
