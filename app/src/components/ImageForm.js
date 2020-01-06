866af3a3e43fac6e1532b3554b150f17b0b7cdd8
Patrick McCarver <patrick@merklebros.com>
2020-01-05 22:57:51 -0500

    Update Imageform

--- app/src/components/ImageForm.js
+++ app/src/components/ImageForm.js
@@ -5,29 +5,49 @@ import uuidv1 from 'uuid/v1';
 export default function ImageForm(props) {
   const [images, setImages] = useState([]);
 
-  const handleChange =
-    event => {
+  const handleBrowseFiles =
+    async(event) => {
       const thumbnailHeight = props.thumbnailHeight
       const files = Array.from(event.target.files)
 
-      const imageObjects = 
-        files
-          .map(file => {
-            const image = new Image();
-            image.src = URL.createObjectURL(file);
-            return { file: file
-                   , src: image.src
-                   , thumbnail: image.src
-                   , thumbnailHeight: thumbnailHeight
-                   , thumbnailWidth: (image.width / image.height) * thumbnailHeight
-    }});
+      const loadedImages = 
+        await Promise.all(files.map(file => loadImage(file)))
 
-    setImages(imageObjects)
+      const imageObjects = 
+        loadedImages.map(
+          image => createImageObject(image.file, image.image, "ready"))
+
+      setImages(imageObjects)
   };
+
+  const createImageObject = (file, image, state) => {
+    return { file: file
+           , image: image
+           , tags: [{value: state, title: "state"}]
+           , src: image.src
+           , thumbnail: image.src
+           , thumbnailHeight: props.thumbnailHeight
+           , thumbnailWidth: (image.width / image.height) * props.thumbnailHeight
+    }};
+
+  const loadImage = (file) => {
+    return new Promise((resolve, reject) => {
+      try {
+        const image = new Image();
+        image.id = uuidv1();
+        image.src = URL.createObjectURL(file);
+        image.onload = () => {
+          resolve({image: image, file: file});
+        }
+      } catch(e) {
+        // TODO: Resolve with placeholder image
+      }
+  })};
 
   const postFileToAPI = 
     async (file) => {
       console.log("Posting file to Serverless API");
+
       return await fetch(
         "https://cors-anywhere.herokuapp.com/" +
         "https://ovr5yyzvza.execute-api.us-east-1.amazonaws.com/dev/generate_ascii", 
@@ -44,55 +64,78 @@ export default function ImageForm(props) {
   const createImageObjectFromResponse =
     async (response) => {
       console.log("Proccessing response from Serverless API");
+      console.log("Response: ", response)
 
       const blob = await response.blob()
       const file = new File([blob], uuidv1())
-      const url = URL.createObjectURL(file)
-      const image = new Image()
-      image.src = url
+      const {image} = await loadImage(file)
 
       console.log("response blob: ", blob)
       console.log("file: ", file)
-      console.log("url: ", url)
 
-      return { file: file
-             , src: image.src
-             , thumbnail: image.src
-             , thumbnailHeight: props.thumbnailHeight
-             , thumbnailWidth: (image.width / image.height) * props.thumbnailHeight
-  }}
+      return createImageObject(file, image, "ascii")
+    };
+
+  const updateImage = image => {
+    setImages(oldImages => {
+      const index = images.indexOf(image)
+      const newImages = [...oldImages]
+      newImages[index] = image;
+      return newImages;
+    })
+  };
 
   const handleSubmit =
     async (event) => {
-      event.preventDefault();
-
       try {
+        event.preventDefault();
+
         for (const image of images) {
-          const response = await postFileToAPI(image.file)
-          const imageObject = await createImageObjectFromResponse(response)
+          try {
 
-          console.log("response object: ", response)
-          console.log("ImageObject inside handleSubmit: ", imageObject)
+            image.tags[0].value = "processing"
+            updateImage(image)
 
-          setImages([...images, imageObject])
+            const response = await postFileToAPI(image.file)
+            console.log("response object: ", response)
+
+            if (response.status !== 200) {
+              image.tags[0].value = "failed"
+              updateImage(image)
+              continue;
+            }
+
+            const imageObject = await createImageObjectFromResponse(response)
+
+            image.tags[0].value = "finished"
+            image["isSelected"] = true
+
+
+            setImages(oldImages => [...oldImages, imageObject])
+
+          } catch(e) {
+            const message = "Error processing file with API: "
+            console.log(message, e)
+            continue;
+          }
         }
       } catch(e) {
-        const message = "Error processing file in API: "
+        const message = "Error processing files with API: "
         console.log(message, e)
       }
   };
 
   return (
-    <>
+    <div>
       <form onSubmit={handleSubmit}>
         <label>
-          Upload images:
+          Upload file
           <input 
+            id='image-input'
             type='file' 
             multiple
             accept='.png, .jpg, .jpeg'
-            id='image-input'
-            onChange={e => handleChange(e)} />
+            onChange={e => handleBrowseFiles(e)} />
         </label>
         <input 
           type="submit" 
@@ -101,6 +144,6 @@ export default function ImageForm(props) {
       <Gallery 
         images={images} 
         backdropClosesModal={true} />
-    </>
+    </div>
   );
 }
