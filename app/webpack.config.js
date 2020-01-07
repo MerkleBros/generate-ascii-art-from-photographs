b5501ed84e503315888c0e215f3577231eb91243
Patrick McCarver <patrick@merklebros.com>
2020-01-06 22:36:51 -0500

    Install css-loader and style-loader
    Allows importing css stylesheets with Webpack.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
-    ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx']
